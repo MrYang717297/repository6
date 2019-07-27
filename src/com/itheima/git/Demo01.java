6229693c9c439714206840ec067ea44fc22871dc
eryagongzi <a1194156316@qq.com>
2019-07-27 17:25:56 +0800

    第一次提交项目

---
+++ src/com/itheima/git/Demo01.java
@@ -0,0 +1,17 @@
+package com.itheima.git;
+
+/**
+ * @author MrYang
+ * @date 2019/7/27 17:17
+ */
+public class Demo01 {
+    public static void main(String[] args) {
+        System.out.println("Hello Git...");
+        System.out.println("Hello Git...");
+        System.out.println("Hello Git...");
+        System.out.println("Hello Git...");
+        System.out.println("Hello Git...");
+        System.out.println("Hello Git...");
+        System.out.println("Hello Git...");
+    }
+}
